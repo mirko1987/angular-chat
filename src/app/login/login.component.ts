137284ed26792004b46d6850155ae7a638ba9f1f
mirko uriel <mirkouriel@MBP-di-mirko.homenet.telecomitalia.it>
2020-06-26 17:39:36 +0200

    upgrade login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,14 @@ import {FormControl,FormGroupDirective,FormBuilder,FormGroup,NgForm,Validators} 
 import {ErrorStateMatcher} from '@angular/material/core';
 import * as firebase from 'firebase';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,12 +22,34 @@ export class LoginComponent implements OnInit {
   loginForm:FormGroup;
   nickname='';
   ref=firebase.database().ref('users/');
-  matcher =new MyErrorStateMatcher();
+  matcher = new MyErrorStateMatcher();
 
 
   constructor(private router:Router, private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    if(localStorage.getItem('nickname')){
+      this.router.navigate(['/roomlist']);
+    }
+    this.loginForm = this.formBuilder.group({
+      'nickname': [null,Validators.required]
+    });
   }
+
+  onFormSubmit(form: any) {
+    const login = form;
+    this.ref.orderByChild('nickname').equalTo(login.nickname).once('value', snapshot => {
+      if (snapshot.exists()) {
+        localStorage.setItem('nickname', login.nickname);
+        this.router.navigate(['/roomlist']);
+      } else {
+        const newUser = firebase.database().ref('users/').push();
+        newUser.set(login);
+        localStorage.setItem('nickname', login.nickname);
+        this.router.navigate(['/roomlist']);
+      }
+    });
+  }
+
 
 }
